6ec7469def7235c32f59b8b2afb087bc9601ce80
Milan <Milan>
2016-07-07 16:31:29 +0200

    Changing approach from trigonometry to vectors

--- src/Main.java
+++ src/Main.java
@@ -17,29 +17,67 @@ public class Main {
 //		DirectoryPicturesLoader parser = new DirectoryPicturesLoader("Invalidovna, ", "C:/Users/Milan/Google Drive/Škola/Magistr/Diplomka2/bkpImages/invalidovna");
 //		PictureTelemetryDao dao = PictureTelemetryDao.getInstance();
 		
-		final double altitude = 30;
-		final double angleX = Math.toRadians(40);
-		final double angleY = Math.toRadians(50);
+		final double altitude = 20;
+		final double angleX = Math.toRadians(0);
+		final double angleY = Math.toRadians(0);
 		final double FOVWidth = Math.toRadians(64.4);
+//		final double FOVWidth = Math.toRadians(80);
 		final double FOVHeight = Math.toRadians(37.2);
 		
-		System.out.println("Bottom: " + CameraCalculator.droneToBottomDist(altitude, FOVWidth, angleX));
-		System.out.println("Top: " + CameraCalculator.droneToTopDist(altitude, FOVWidth, angleX));
+		Vector3d ray1 = CameraCalculator.ray1(FOVWidth, FOVHeight);
+		Vector3d ray2 = CameraCalculator.ray2(FOVWidth, FOVHeight);
+		Vector3d ray3 = CameraCalculator.ray3(FOVWidth, FOVHeight);
+		Vector3d ray4 = CameraCalculator.ray4(FOVWidth, FOVHeight);
+		CameraCalculator.printDirections(ray1, ray2, ray3, ray4);
 		
-		System.out.println("Left: " + CameraCalculator.droneToLeftDist(altitude, FOVHeight, angleY));
-		System.out.println("Right: " + CameraCalculator.droneToRightDist(altitude, FOVHeight, angleY));
+		Vector3d [] rotatedVectors = CameraCalculator.rotateRays(
+				ray1, ray2, ray3, ray4, Math.toRadians(0), Math.toRadians(0), Math.toRadians(0));
+		ray1 = rotatedVectors[0];
+		ray2 = rotatedVectors[1];
+		ray3 = rotatedVectors[2];
+		ray4 = rotatedVectors[3];
 		
-//		Vector2d pivot = new Vector2d(0, 0);
-//		Vector2d point = new Vector2d(5, 0);
+		Vector3d intersection1 = CameraCalculator.findVectorGroundIntersection(ray1, new Vector3d(0, 0, 20));
+		Vector3d intersection2 = CameraCalculator.findVectorGroundIntersection(ray2, new Vector3d(0, 0, 20));
+		Vector3d intersection3 = CameraCalculator.findVectorGroundIntersection(ray3, new Vector3d(0, 0, 20));
+		Vector3d intersection4 = CameraCalculator.findVectorGroundIntersection(ray4, new Vector3d(0, 0, 20));
+		System.out.println("Intersecting: " + intersection1.x + ", " + intersection1.y + ", " + intersection1.z);
+		System.out.println("Intersecting: " + intersection2.x + ", " + intersection2.y + ", " + intersection2.z);
+		System.out.println("Intersecting: " + intersection3.x + ", " + intersection3.y + ", " + intersection3.z);
+		System.out.println("Intersecting: " + intersection4.x + ", " + intersection4.y + ", " + intersection4.z);
+		
+//		System.out.println("Bottom: " + CameraCalculator.droneToBottomDist(altitude, FOVWidth, angleX));
+//		System.out.println("Top: " + CameraCalculator.droneToTopDist(altitude, FOVWidth, angleX));
 //		
-//		System.out.println(CameraCalculator.rotatePoint(point, Math.toRadians(0), pivot));
-//		System.out.println(CameraCalculator.rotatePoint(point, Math.toRadians(90), pivot));
-//		System.out.println(CameraCalculator.rotatePoint(point, Math.toRadians(180), pivot));
-//		System.out.println(CameraCalculator.rotatePoint(point, Math.toRadians(270), pivot));
-		
+//		System.out.println("Left: " + CameraCalculator.droneToLeftDist(altitude, FOVHeight, angleY));
+//		System.out.println("Right: " + CameraCalculator.droneToRightDist(altitude, FOVHeight, angleY));
 		
 		
 		System.out.println("Program has finished");
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/geometry/CameraCalculator.java
+++ src/geometry/CameraCalculator.java
@@ -1,9 +1,94 @@
 package geometry;
 
+import javax.vecmath.Matrix3d;
 import javax.vecmath.Vector2d;
+import javax.vecmath.Vector3d;
+
+import Jama.Matrix;
 
 public class CameraCalculator {
 	
+	public static Vector3d ray1(double FOVWidth, double FOVHeight) {
+		return new Vector3d(1, Math.tan(FOVWidth), Math.tan(FOVHeight));
+	}
+	
+	public static Vector3d ray2(double FOVWidth, double FOVHeight) {
+		return new Vector3d(1, -Math.tan(FOVWidth), Math.tan(FOVHeight));
+	}
+	
+	public static Vector3d ray3(double FOVWidth, double FOVHeight) {
+		return new Vector3d(1, Math.tan(FOVWidth), -Math.tan(FOVHeight));
+	}
+	
+	public static Vector3d ray4(double FOVWidth, double FOVHeight) {
+		return new Vector3d(1, -Math.tan(FOVWidth), -Math.tan(FOVHeight));
+	}
+	
+	public static void printDirections(Vector3d ray1,Vector3d ray2, Vector3d ray3, Vector3d ray4) {
+		System.out.println("(" + ray1.x + ", " + ray1.y + ", " + ray1.z + ")");
+		System.out.println("(" + ray2.x + ", " + ray2.y + ", " + ray2.z + ")");
+		System.out.println("(" + ray3.x + ", " + ray3.y + ", " + ray3.z + ")");
+		System.out.println("(" + ray4.x + ", " + ray4.y + ", " + ray4.z + ")");
+	}
+	
+	public static Vector3d[] rotateRays(Vector3d ray1,Vector3d ray2, Vector3d ray3, Vector3d ray4, double roll, double pitch, double yaw) {
+		double sinAlpha = Math.sin(roll);
+		double sinBeta = Math.sin(pitch);
+		double sinGamma = Math.sin(yaw);
+		double cosAlpha = Math.cos(roll);
+		double cosBeta = Math.cos(pitch);
+		double cosGamma = Math.cos(yaw);
+		double m00 = cosAlpha * cosBeta;
+		double m01 = cosAlpha * sinBeta * sinGamma - sinAlpha * cosGamma;
+		double m02 = cosAlpha * sinBeta * cosGamma + sinAlpha * sinGamma;
+		double m10 = sinAlpha * cosBeta;
+		double m11 = sinAlpha * sinBeta * sinGamma + cosAlpha * cosGamma;
+		double m12 = sinAlpha * sinBeta * cosGamma - cosAlpha * sinGamma;
+		double m20 = -sinBeta;
+		double m21 = cosBeta * sinGamma;
+		double m22 = cosBeta * cosGamma;
+		
+		// Using an external library to rotate vectors
+		Matrix rotationMatrix = new Matrix(new double[][]{{m00, m01, m02}, {m10, m11, m12}, {m20, m21, m22}});
+		Matrix ray1Matrix = new Matrix(new double[][]{{ray1.x, 0, 0}, {ray1.y, 0, 0}, {ray1.z, 0, 0}});
+		Matrix ray2Matrix = new Matrix(new double[][]{{ray2.x, 0, 0}, {ray2.y, 0, 0}, {ray2.z, 0, 0}});
+		Matrix ray3Matrix = new Matrix(new double[][]{{ray3.x, 0, 0}, {ray3.y, 0, 0}, {ray3.z, 0, 0}});
+		Matrix ray4Matrix = new Matrix(new double[][]{{ray4.x, 0, 0}, {ray4.y, 0, 0}, {ray4.z, 0, 0}});
+		
+		Matrix res1 = rotationMatrix.times(ray1Matrix);
+		Matrix res2 = rotationMatrix.times(ray2Matrix);
+		Matrix res3 = rotationMatrix.times(ray3Matrix);
+		Matrix res4 = rotationMatrix.times(ray4Matrix);
+		
+		System.out.println();
+		System.out.println("(" + res1.get(0, 0) + ", " + res1.get(1, 0) + ", " + res1.get(2, 0) + ")");
+		System.out.println("(" + res2.get(0, 0) + ", " + res2.get(1, 0) + ", " + res2.get(2, 0) + ")");
+		System.out.println("(" + res3.get(0, 0) + ", " + res3.get(1, 0) + ", " + res3.get(2, 0) + ")");
+		System.out.println("(" + res4.get(0, 0) + ", " + res4.get(1, 0) + ", " + res4.get(2, 0) + ")");
+		
+		return new Vector3d[]{new Vector3d(res1.get(0, 0), res1.get(1, 0), res1.get(2, 0)),
+				new Vector3d(res2.get(0, 0), res2.get(1, 0), res2.get(2, 0)),
+				new Vector3d(res3.get(0, 0), res3.get(1, 0), res3.get(2, 0)),
+				new Vector3d(res4.get(0, 0), res4.get(1, 0), res4.get(2, 0))};
+	}
+	
+	public static Vector3d findVectorGroundIntersection(Vector3d vector, Vector3d origin) {
+		
+		// Parametric form of an equation
+		// P = origin + vector * t
+		Vector2d x = new Vector2d(origin.x,vector.x);
+		Vector2d y = new Vector2d(origin.y,vector.y);
+		Vector2d z = new Vector2d(origin.z,vector.z);
+		
+		// Equation of the horizontal plane (ground)
+		// -z = 0
+		
+		// Calculate t by substituting z
+		double t = - (z.x / z.y);
+		
+		// Substitute t in the original parametric equations to get points of intersection
+		return new Vector3d(x.x + x.y * t, y.x + y.y * t, z.x + z.y * t);
+	}
 	
 	public static Trapezoid getTrapezoid(double altitude, double FOVWidth, double FOVHeight, double heading, double roll, double pitch) {
 		double bottom = droneToBottomDist(altitude, FOVWidth, pitch);
@@ -52,6 +137,9 @@ public class CameraCalculator {
 			return angle;
 		}
 	}
+	
+	
+	
 	
 	public static Vector2d translatePoint(double x, double y, double translationX, double translationY) {
 		return new Vector2d(x + translationX, y + translationY);
